d452af4b83e4cb0f8d0094bf1e0c1b407d39bdf5
cheburakshu <cheburakshu@gmail.com>
2018-02-11 16:57:03 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import codecs
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Get the version
+version_regex = r'__version__ = ["\']([^"\']*)["\']'
+with open('fly/__init__.py', 'r') as f:
+    text = f.read()
+    match = re.search(version_regex, text)
+
+    if match:
+        version = match.group(1)
+    else:
+        raise RuntimeError("No version number found!")
+
+# Stealing this from Kenneth Reitz
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'fly',
+]
+
+setup(
+    name='fly',
+    version=version,
+    description='Asynchronous Dataflow programming framework',
+    author='Sudharsan R',
+    packages=packages,
+    package_dir={'fly': 'fly'},
+    #install_requires=[
+    #    'kafka-python',
+    #],
+    #extras_require={
+    #    ':python_version == "2.7" or python_version == "3.3"': ['enum34>=1.1.6, <2'],
+    #}
+)
